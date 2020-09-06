4829440f0660454088c680227ae1bf6a93d82951
Michael Dokolin <m@dokol.in>
2020-09-06 15:04:37 +0200

    Add DOM render function

--- src/renderer/dom/index.ts
+++ src/renderer/dom/index.ts
@@ -1,1 +1,16 @@
+import { Children, render as $render } from 'mvvm.js';
+import { Elements } from './elements';
+import { XhtmlRenderer } from './xhtml-renderer';
+
+const renderer = new XhtmlRenderer();
+
+/**
+ * Render a Virtual DOM element.
+ * @param children - The Virtual DOM elements to render.
+ * @param container - Target DOM element.
+ */
+export function render(children: Children<Elements>, container: Document | Element): void {
+  $render(renderer, children, container);
+}
+
 export * from './elements';
