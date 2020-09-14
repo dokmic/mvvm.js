1eacc66b31f4eaae34981ec24cb693c6be2703e4
Michael Dokolin <m@dokol.in>
2020-09-14 06:58:11 +0200

    Add demo application layout

---
+++ demo/src/global.d.ts
@@ -0,0 +1,1 @@
+import 'mvvm.js/lib/dom/jsx';
---
+++ demo/src/index.tsx
@@ -0,0 +1,7 @@
+import { createElement } from 'mvvm.js';
+import { render } from 'mvvm.js/lib/dom';
+
+render(
+  <div />,
+  document.querySelector('#root')!,
+);
