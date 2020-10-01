811d0dce1c431cc20646ae9192c2108c99ef6e9f
Michael Dokolin <m@dokol.in>
2020-10-01 21:12:42 +0200

    Update demo project to render a formatted text with the toolbar

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,7 +1,17 @@
-import { createElement } from 'mvvm.js';
+import { Fragment, createElement } from 'mvvm.js';
 import { render } from 'mvvm.js/lib/dom';
+import { Toolbar, Text } from './components';
+
+const font: Font = { align: 'center', family: 'courier', size: 30 };
 
 render(
-  <div />,
+  <>
+    <div class="mt-3">
+      <Toolbar font={font} />
+    </div>
+    <div class="my-auto">
+      <Text font={font}>Hello, MVVM.js</Text>
+    </div>
+  </>,
   document.querySelector('#root')!,
 );
